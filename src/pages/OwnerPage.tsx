7770ea494aeb4d65673594356942aed91b790991
Zombox <ghostzb@outlook.com>
2024-11-06 12:28:39 +0700

    create OwnerPage

--- src/pages/OwnerPage.tsx
+++ src/pages/OwnerPage.tsx
@@ -1,8 +1,23 @@
-
 function OwnerPage() {
   return (
-    <main>OwnerPage</main>
-  )
+    <main className="flex flex-col items-center justify-evenly w-full h-screen py-16 px-8">
+      <div>
+        <h1>Earth - Group D - 08</h1>
+      </div>
+      <img
+        src="https://scontent.fbkk19-1.fna.fbcdn.net/v/t39.30808-6/463908782_596472892714734_7924920922275408467_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=5dcLDN3fA30Q7kNvgFaoc1W&_nc_zt=23&_nc_ht=scontent.fbkk19-1.fna&_nc_gid=ATSBGldfq4P4zPqdY6yiNYW&oh=00_AYCssrh6z1xWIgYNOnaU0oRQgC3lN9W1_dUvg0liblkK8A&oe=6730E742"
+        alt="monkey"
+        className=" w-96  border-8 border-black"
+      />
+      <div className=" flex flex-col items-center gap-y-8 justify-center ">
+        <h3 className=" text-xl font-medium">Short Biography 🐵</h3>
+        <p className=" text-base">
+          🍌 อุ้ ๆ อ่ ๆ อุๆ อะๆ อุๆ อ๊าาาๆ อุๆๆ อุกก้าาา อุ้ ๆ อ่ ๆ อุๆ อะๆ อุๆ
+          อ๊าาาๆ อุๆๆ อุกก้าาา อุ้ ๆ อ่ ๆ อุๆ อะๆ อุๆ อ๊าาาๆ อุๆๆ อุกก้าาา 🍌
+        </p>
+      </div>
+    </main>
+  );
 }
 
-export default OwnerPage
+export default OwnerPage;
