9f7e1774dce9fbe3d38b4e2448cebb17bbf8764a
Zombox <ghostzb@outlook.com>
2024-11-06 09:18:44 +0700

    edit deployment error

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
   return (
     <div className=" flex flex-col items-center justify-evenly w-full min-h-screen py-16 px-8">
       <div className=" flex flex-col items-center gap-y-6">
@@ -8,8 +10,8 @@ function HomePage() {
         <h1>React - Assessment</h1>
       </div>
       <div className=" w-full flex justify-center gap-x-32">
-        <button>User Home Sector</button>
-        <button>Admin Home Sector</button>
+        <button onClick={() => navigate("/user")}>User Home Sector</button>
+        <button onClick={() => navigate("/admin")}>Admin Home Sector</button>
       </div>
     </div>
   );
